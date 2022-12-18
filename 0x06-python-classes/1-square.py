2246ef44f49674e47c10ee9faed8c416138c1e8f
CharlyAnne <icharlieanne@gmail.com>
2022-12-19 04:36:08 +0530

    classes tasks

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a Square."""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size (init): The size of the new Square.
+        """
+        self.__size = size
