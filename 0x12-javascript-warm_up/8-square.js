7aacfa3a8ebfa6f84d523451a684127b0f33789b
CharlyAnne <icharlieanne@gmail.com>
2023-03-14 00:50:17 +0100

    task 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const x = process.argv[2];
+
+if (!parseInt(x)) {
+    console.log('Missing size');
+} else {
+    for (let i = 0; i < x; i++) {
+        let z = 0;
+        let myVar = '';
+
+        while (z < x) {
+            myVar = myVar + 'X';
+            z++;
+        }
+        console.log(myVar);
+    }
+}
