529518e7c84c4b1f26f331b3b8a003db048c0357
CharlyAnne <icharlieanne@gmail.com>
2023-03-13 22:19:00 +0100

    task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 
 const number = Math.trunc(process.argv[2]);
 if (isNaN(number)) {
-	console.log('Not a number');
+  console.log('Not a number');
 } else {
-	console.log(`My number: ${number}`);
+  console.log(`My number: ${number}`);
 }
