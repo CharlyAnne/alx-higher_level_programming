94148fe64626f35f1d2b7173dc75d5b50116140c
CharlyAnne <icharlieanne@gmail.com>
2023-03-14 22:33:52 +0000

    task 101(Advanced)

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const dict = require('./101-data').dict;
+
+const newDictOccurrence = {};
+for (const [key, value] of Object.entries(dict)) {
+  if (newDictOccurrence[value]) {
+  newDictOccurrence[value].push(key);
+} else {
+  (newDictOccurrence[value] = [key]);
+}
+}
+console.log(newDictOccurrence);
