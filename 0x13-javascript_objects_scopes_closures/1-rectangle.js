c8bdf00f61d18edf0ca59786c326182b6b0b8899
CharlyAnne <icharlieanne@gmail.com>
2023-03-14 13:52:12 +0000

    task 2

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,9 +1,9 @@
-//#!/usr/bin/node
+// #!/usr/bin/node
 
 class Rectangle {
   construct (w, h) {
-  this.width = w;
-  this.height = h;
+    this.width = w;
+    this.height = h;
   }
 }
 module.exports = Rectangle;
