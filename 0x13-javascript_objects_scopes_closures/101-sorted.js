8ba2bf13877c75110f86aa1559e647ac3b940026
CharlyAnne <icharlieanne@gmail.com>
2023-03-14 23:50:50 +0100

    task 101

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -4,9 +4,9 @@ const dict = require('./101-data').dict;
 const newDictOccurrence = {};
 for (const [key, value] of Object.entries(dict)) {
   if (newDictOccurrence[value]) {
-  newDictOccurrence[value].push(key);
-} else {
-  (newDictOccurrence[value] = [key]);
-}
+    newDictOccurrence[value].push(key);
+  } else {
+    (newDictOccurrence[value] = [key]);
+  }
 }
 console.log(newDictOccurrence);
