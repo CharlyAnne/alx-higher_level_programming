812eda491a87ea1dab2b1182ead34d6ecca0b846
CharlyAnne <icharlieanne@gmail.com>
2023-03-15 23:29:21 +0100

    task 13

---
+++ 0x13-javascript_objects_scopes_closures/test.js
@@ -0,0 +1,14 @@
+//#!/usr/bin/node
+const dict = require('./101-data').dict;
+
+const newDict = (dict) => {
+    const newDiction= {};
+    for (const key in dict) {
+        if (newDiction[dict[key]] === undefined) {
+            newDiction[dict[key]] = [];//array/list
+        }
+        newDiction[dict[key]].push(key)
+    }
+    return newDiction;
+};
+console.log(newDict(dict));
