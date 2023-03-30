89123c50f325b4cb74ee7b35ac06520d999f2168
Charlian <100117263+CharlyAnne@users.noreply.github.com>
2023-03-30 21:43:33 +0100

    Get request:fix
    Followed the rule of having just 3 lines

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# this script takes in a URL, sends a GET request to the URL,
-# and displays the body of the response.
-
+# takes in a URL, sends a GET request and displays the body of the response.
 curl -sL $1 -X GET
