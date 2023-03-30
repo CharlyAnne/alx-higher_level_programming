b9fc555231b3dbbcfa5450f6daf1f963e6ee8e7f
Charlian <100117263+CharlyAnne@users.noreply.github.com>
2023-03-30 21:37:40 +0100

    Response body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# this bash script takes in a URL, sends a request to the URL and 
-# displays size of the body response.
-
+# takes in a URL, sends a request to the URL and displays size of the body response.
 curl -sI "$1" | grep -i "Content-length" | cut -d ":" -f 2
