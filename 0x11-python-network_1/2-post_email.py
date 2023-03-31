64a316cfcf25d8a24fac14419768bf537bfe2af1
Charlian <100117263+CharlyAnne@users.noreply.github.com>
2023-03-31 17:14:57 +0100

    post email:quick fix

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,11 +7,13 @@ import sys
 import urllib.request
 import urllib.parse
 
-if __name__ == "__main__":
-    url = sys.argv[1]
-    value = {"email": sys.argv[2]}
-    data = urllib.parse.urlencode(value).encode("ascii")
+url = sys.argv[1]
+email = {'email': sys.argv[2]}
 
-request = urllib.request.Request(url, data)
-with urllib.request.urlopen(request) as response:
-    print(response.read().decode("uft-8"))
+data = urllib.parse.urlencode(email)
+data = data.encode('ascii')
+req = urllib.request.Request(url, data)
+
+with urllib.request.urlopen(req) as response:
+    byte = response.read()
+print(byte.decode())
