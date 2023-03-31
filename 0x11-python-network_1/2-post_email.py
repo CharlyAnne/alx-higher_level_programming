eb6d4ada21d5103e51a243ae1d66941feaa6c910
CharlyAnne <icharlieanne@gmail.com>
2023-03-31 16:50:32 +0100

    post email

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""script takes in a URL and an email, sends a POST request to the passed URL
+    with the email as a parameter, to display the response body.
+Usage: ./2-post_email.py <URL> <email>
+"""
+import sys
+import urllib.request
+import urllib.parse
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode("ascii")
+
+request = urllib.request.Request(url, data)
+with urllib.request.urlopen(request) as response:
+    print(response.read().decode("uft-8"))
