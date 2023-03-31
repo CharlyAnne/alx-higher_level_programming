9e332b5cc72aabcd6cb7ac7ffbf454861a185e42
Charlian <100117263+CharlyAnne@users.noreply.github.com>
2023-03-31 23:22:04 +0100

    Post email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,12 +9,12 @@ import urllib.parse
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    email = {'email': sys.argv[2]}
+    value = {'email': sys.argv[2]}
 
-    data = urllib.parse.urlencode(email)
-    data = data.encode('ascii')
+    data = urllib.parse.urlencode(value).encode('ascii')
     req = urllib.request.Request(url, data)
 
     with urllib.request.urlopen(req) as response:
         byte = response.read()
         print(byte.decode())
+    
