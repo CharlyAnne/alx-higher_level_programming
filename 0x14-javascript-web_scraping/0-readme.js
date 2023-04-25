100924a9b4896ef6de51c3b5cd868fba86b58f83
CharlyAnne <icharlieanne@gmial.com>
2023-04-25 14:23:12 +0000

    reads and prints content

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 
-// Import built-in 'fs' module
 const fs = require('fs');
 const filename = process.argv[2];
 
 fs.readFile(filename, 'utf-8', (error, data) => {
-	if (error) {
-	  console.log(error);
-	} else {
-	  console.log(data);
-	}
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(data);
+  }
 });
