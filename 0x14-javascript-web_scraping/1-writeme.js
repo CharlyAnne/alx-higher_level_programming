d9b408aacff45dbd455f9e4f87554b7030867378
CharlyAnne <icharlieanne@gmial.com>
2023-04-25 14:33:15 +0000

    writes string to file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const fiename = process.argv[2];
 const data = process.argv[3];
 
-fs.writeFile('filename, data, 'utf-8', (error) => {
+fs.writeFile(filename, data, 'utf-8', (error) => {
   if (error) {
     console.log(error);
   }
